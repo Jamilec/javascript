44fab791ffe399f0c217bb0f2390ffac146bb084
Jamile Caroline <jamilycaroline18@gmail.com>
2021-01-07 12:11:26 -0300

    fiz o miniprojeto14.js

---
+++ miniprojeto14.js
@@ -0,0 +1,6 @@
+let faixas = [
+    {tooltip: 'até R$700', minimo: 0, maximo: 700},
+    {tooltip: 'de R$700 até R$1000', minimo: 700, maximo: 1000},
+    {tooltip: 'R$1000 ou mais', minimo: 1000, maximo: 999999}
+];
+console.log(faixas)
