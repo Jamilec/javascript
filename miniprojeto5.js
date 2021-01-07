c5d1dc822260f57ad61608b6e2434b54fdb9b2c6
Jamile Caroline <jamilycaroline18@gmail.com>
2021-01-07 11:52:18 -0300

    fiz o miniprojeto5.js

---
+++ miniprojeto5.js
@@ -0,0 +1,10 @@
+exibirTipo (1222);
+
+function exibirTipo(limite) {
+    for(let i = 0; i<= limite; i++) {
+        if (i % 2 === 0)
+            console.log(i,'PAR');
+        else 
+            console.log (i,'IMPAR');
+    }
+}
